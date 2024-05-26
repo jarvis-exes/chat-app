318435973759fd1cd82ebe3f26d4898c5f6e4f81
Shubham Sharma <8696771553shubham@gmail.com>
2024-05-26 20:18:31 +0530

    Completed Chat Component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
+
+const handleEmoji = (e) => {
+  setText((prev) => prev + e.emoji);
+  setOpen(false);
+  console.log(text);
+};
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+    console.log(text);
+  };
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Shubham Sharma</span>
+            <p>Hello this is shubham's profile</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ea
+              natus sequi ullam tenetur atque quae repellat repellendus itaque,
+              quisquam doloremque exercitationem, voluptas possimus aperiam eius
+              necessitatibus aspernatur quam vel.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img
+              src="https://images3.alphacoders.com/135/thumbbig-135625.webp"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ea
+              natus sequi ullam tenetur atque quae repellat repellendus itaque,
+              quisquam doloremque exercitationem, voluptas possimus aperiam eius
+              necessitatibus aspernatur quam vel.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chatList.css";
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
+
   return (
     <div className="chatList">
       <div className="search">
@@ -9,7 +11,40 @@ const ChatList = () => {
           <img src="./search.png" alt="" />
           <input type="text" placeholder="Search" />
         </div>
-        <img src="./plus.png" alt="" />
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Shubham Sharma</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Shubham Sharma</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Shubham Sharma</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Shubham Sharma</span>
+          <p>Hello</p>
+        </div>
       </div>
     </div>
   );
