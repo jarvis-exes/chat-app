205085667986322eb5b7d6388ad27e7eb20fcd25
Shubham Sharma <8696771553shubham@gmail.com>
2024-06-04 23:51:02 +0530

    Sign In and Log out working

--- src/components/detail/Details.jsx
+++ src/components/detail/Details.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./details.css";
+import { auth } from "../../lib/firebase";
 
 const Details = () => {
   return (
@@ -67,7 +68,9 @@ const Details = () => {
           </div>
         </div>
         <button>Block User</button>
-        <button className="logout">Logout</button>
+        <button className="logout" onClick={() => auth.signOut()}>
+          Logout
+        </button>
       </div>
     </div>
   );
